cf8b7e8b3e4ad8365a3edc84d51b603b464506bb
aguuuppyy <your_email@example.com>
2025-09-17 13:24:02 +0800

    feat(derived-aguuuppyy): add derived with unique property and method

---
+++ lib/mage.dart
@@ -0,0 +1,22 @@
+import 'character.dart';
+
+class Mage extends Character {
+  int mana;
+
+  Mage(String name, int level, int health, this.mana)
+      : super(name, level, health);
+
+  void castSpell(String spell) {
+    if (mana < 10) {
+      print("$name is too tired to cast $spell!");
+    } else {
+      mana -= 10;
+      print("$name casts $spell! Mana left: $mana");
+    }
+  }
+
+  @override
+  String describe() {
+    return "Mage ${super.describe()} | Mana: $mana";
+  }
+}
