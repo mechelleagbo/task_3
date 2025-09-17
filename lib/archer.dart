ed002a3406f06f9b9fa55ee8032f40438d0aba6b
aguuuppyy <your_email@example.com>
2025-09-17 13:38:14 +0800

    features(derived-aguuuppy): add derived with unique property and method

---
+++ lib/archer.dart
@@ -0,0 +1,22 @@
+import 'character.dart';
+
+class Archer extends Character {
+  int arrows;
+
+  Archer(String name, int level, int health, this.arrows)
+      : super(name, level, health);
+
+  void shootArrow() {
+    if (arrows > 0) {
+      arrows--;
+      print("$name shoots an arrow! Arrows left: $arrows");
+    } else {
+      print("$name has no arrows left!");
+    }
+  }
+
+  @override
+  String describe() {
+    return "Archer ${super.describe()} | Arrows: $arrows";
+  }
+}
